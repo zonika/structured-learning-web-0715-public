8b8a1fbc8834a0ac53bb31eed223f92d536a0f56
Spencer Rogers <spencer1248@gmail.com>
2013-10-14 18:13:32 -0400

    Add config/env presence test

--- spec/file_structure_spec.rb
+++ spec/file_structure_spec.rb
@@ -1,7 +1,13 @@
-describe "conventional ruby file structure" do
+describe "a conventional ruby project structure" do
   describe "tests" do
     it "should require a helper to load other dependencies" do
       $LOADED_FEATURES.grep(/spec_helper.rb/).any?.should == true
+    end
+  end
+
+  describe "config directory" do
+    it "should have an environment file" do
+      File.exists?(File.join(File.dirname(__FILE__), '../config/env.rb')).should == true
     end
   end
 end
