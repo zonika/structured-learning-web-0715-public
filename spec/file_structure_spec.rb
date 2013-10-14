81d7e5e9a55f70aff07b10f0a4782dc4077983aa
Spencer Rogers <spencer1248@gmail.com>
2013-10-14 18:18:24 -0400

    Test for env.rb requirement

--- spec/file_structure_spec.rb
+++ spec/file_structure_spec.rb
@@ -1,13 +1,17 @@
 describe "a conventional ruby project structure" do
   describe "tests" do
     it "should require a helper to load other dependencies" do
-      $LOADED_FEATURES.grep(/spec_helper.rb/).any?.should == true
+      $LOADED_FEATURES.grep(/spec_helper\.rb/).any?.should == true
     end
   end
 
   describe "config directory" do
     it "should have an environment file" do
       File.exists?(File.join(File.dirname(__FILE__), '../config/env.rb')).should == true
+    end
+
+    it "should have it's environment file required in the spec_helper" do
+      $LOADED_FEATURES.grep(/env\.rb/).any?.should == true
     end
   end
 end
