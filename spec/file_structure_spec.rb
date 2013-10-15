9f31b0ab0ecfaced5d55a84d593c3604e4c57ebf
Spencer Rogers <spencer1248@gmail.com>
2013-10-15 14:57:01 -0400

    Update spec verbiage

--- spec/file_structure_spec.rb
+++ spec/file_structure_spec.rb
@@ -1,40 +1,40 @@
-describe "a conventional project" do
-  describe "tests" do
-    it "should require a spec_helper in each test file to load other dependencies" do
+describe "In a typical project" do
+  describe "the tests" do
+    it "would require a spec_helper in each test file to load other dependencies" do
       $LOADED_FEATURES.grep(/spec\/spec_helper\.rb/).any?.should == true
     end
   end
 
-  describe "lib directory" do
-    it "should exist" do
+  describe "the lib directory" do
+    it "would exist" do
       Dir.exists?(File.join(File.dirname(__FILE__), '../lib/')).should == true
     end
 
-    it "should contain the application code" do
+    it "would contain the application code" do
       File.exists?(File.join(File.dirname(__FILE__), '../lib/foo.rb')).should == true
       File.exists?(File.join(File.dirname(__FILE__), '../lib/bar.rb')).should == true
     end
   end
 
-  describe "config directory" do
-    it "should exist in the application root" do
+  describe "the config directory" do
+    it "would exist in the application root" do
       Dir.exists?(File.join(File.dirname(__FILE__), '../config/')).should == true
     end
 
-    it "should contain an environment file" do
+    it "would contain an environment file" do
       File.exists?(File.join(File.dirname(__FILE__), '../config/environment.rb')).should == true
     end
 
-    describe "environment file" do
-      it "should load files from the lib directory" do
+    describe "the environment file" do
+      it "would load files from the lib directory" do
         File.read(File.join(File.dirname(__FILE__), '../config/environment.rb')).scan(/require/).any?.should == true
         File.read(File.join(File.dirname(__FILE__), '../config/environment.rb')).scan(/lib/).any?.should == true
       end
     end
   end
 
-  describe "spec_helper" do
-    it "should load the environment file" do
+  describe "the spec_helper" do
+    it "would load the environment file" do
       $LOADED_FEATURES.grep(/config\/environment\.rb/).any?.should == true
 
       spec_helper_file = File.read(File.join(File.dirname(__FILE__), 'spec_helper.rb'))
@@ -47,17 +47,17 @@ describe "a conventional project" do
     end
   end
 
-  describe "bin directory" do
-    it "should exist in the application root" do
+  describe "the bin directory" do
+    it "would exist in the application root" do
       Dir.exists?(File.join(File.dirname(__FILE__), '../bin/')).should == true
     end
 
-    it "should contain a run.rb file" do
+    it "would contain a run.rb file" do
       File.exists?(File.join(File.dirname(__FILE__), '../bin/run.rb')).should == true
     end
 
-    describe "run.rb executable" do
-      it "should load the environment" do
+    describe "an executable (in this case run.rb)" do
+      it "would load the environment" do
         run_file_contents = File.read(File.join(File.dirname(__FILE__), '../bin/run.rb'))
 
         run_file_contents.scan(/require/).any?.should == true
