3035c5a2af6862e7435cda4732fd3335207d4011
ahimmelstoss <ahimmelstoss@gmail.com>
2014-06-23 11:38:28 -0400

    rspec 3 syntax and config

--- spec/file_structure_spec.rb
+++ spec/file_structure_spec.rb
@@ -1,46 +1,48 @@
+require 'spec_helper'
+
 describe "In a typical project" do
   describe "the tests" do
     it "would require a spec_helper in each test file to load other dependencies" do
-      $LOADED_FEATURES.grep(/spec\/spec_helper\.rb/).any?.should == true
+      expect($LOADED_FEATURES.grep(/spec\/spec_helper\.rb/).any?).to eq(true)
     end
   end
 
   describe "the lib directory" do
     it "would exist in the application root" do
-      Dir.exists?(File.join(File.dirname(__FILE__), '../lib/')).should == true
+      expect(Dir.exists?(File.join(File.dirname(__FILE__), '../lib/'))).to eq(true)
     end
 
     it "would contain the application code" do
-      File.exists?(File.join(File.dirname(__FILE__), '../lib/foo.rb')).should == true
-      File.exists?(File.join(File.dirname(__FILE__), '../lib/bar.rb')).should == true
+      expect(File.exists?(File.join(File.dirname(__FILE__), '../lib/foo.rb'))).to eq(true)
+      expect(File.exists?(File.join(File.dirname(__FILE__), '../lib/bar.rb'))).to eq(true)
     end
   end
 
   describe "the config directory" do
     it "would exist in the application root" do
-      Dir.exists?(File.join(File.dirname(__FILE__), '../config/')).should == true
+      expect(Dir.exists?(File.join(File.dirname(__FILE__), '../config/'))).to eq(true)
     end
 
     it "would contain an environment file" do
-      File.exists?(File.join(File.dirname(__FILE__), '../config/environment.rb')).should == true
+      expect(File.exists?(File.join(File.dirname(__FILE__), '../config/environment.rb'))).to eq(true)
     end
 
     describe "the environment file" do
       it "would load files from the lib directory" do
-        File.read(File.join(File.dirname(__FILE__), '../config/environment.rb')).scan(/require/).any?.should == true
-        File.read(File.join(File.dirname(__FILE__), '../config/environment.rb')).scan(/lib/).any?.should == true
+        expect(File.read(File.join(File.dirname(__FILE__), '../config/environment.rb')).scan(/require/).any?).to eq(true)
+        expect(File.read(File.join(File.dirname(__FILE__), '../config/environment.rb')).scan(/lib/).any?).to eq(true)
       end
     end
   end
 
   describe "the spec_helper" do
     it "would load the environment file" do
-      $LOADED_FEATURES.grep(/config\/environment\.rb/).any?.should == true
+      expect($LOADED_FEATURES.grep(/config\/environment\.rb/).any?).to eq(true)
 
       spec_helper_file = File.read(File.join(File.dirname(__FILE__), 'spec_helper.rb'))
 
-      spec_helper_file.scan(/require/).any?.should == true
-      spec_helper_file.scan(/environment/).any?.should == true
+      expect(spec_helper_file.scan(/require/).any?).to eq(true)
+      expect(spec_helper_file.scan(/environment/).any?).to eq(true)
 
       expect { Foo }.to_not raise_error
       expect { Bar }.to_not raise_error
@@ -49,23 +51,23 @@ describe "In a typical project" do
 
   describe "the bin directory" do
     it "would exist in the application root" do
-      Dir.exists?(File.join(File.dirname(__FILE__), '../bin/')).should == true
+      expect(Dir.exists?(File.join(File.dirname(__FILE__), '../bin/'))).to eq(true)
     end
 
     it "would contain a run.rb file" do
-      File.exists?(File.join(File.dirname(__FILE__), '../bin/run.rb')).should == true
+      expect(File.exists?(File.join(File.dirname(__FILE__), '../bin/run.rb'))).to eq(true)
     end
 
     describe "an executable (in this case run.rb)" do
       it "would load the environment" do
         run_file_contents = File.read(File.join(File.dirname(__FILE__), '../bin/run.rb'))
 
-        run_file_contents.scan(/require/).any?.should == true
-        run_file_contents.scan(/environment/).any?.should == true
+        expect(run_file_contents.scan(/require/).any?).to eq(true)
+        expect(run_file_contents.scan(/environment/).any?).to eq(true)
       end
 
       it "should call run on the Foo class" do
-        Foo.should_receive(:run)
+        expect(Foo).to receive(:run)
 
         require_relative '../bin/run.rb'
       end
